2771b06da45f330ba7638144f0cb8571c358a7f3
bbhitec <bboy.hitec@gmail.com>
2024-03-14 21:46:40 +0200

    server access point - hardcoded

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+/**
+ *
+ * @author vnikolin
+ *
+ */
+
 // define the server access port
 const PORT = 8000
 
 // include all needed packages
 import express from 'express'
 import cors from 'cors'
-// const express = require('express')
-// const cors = require('cors')
 
 // initialize the express server
 const app = express()
@@ -15,5 +19,35 @@ app.use(express.json())
 app.use(cors())
 
 
+
+
+
+
+// define backend access point
+app.post('/completions', async (req, res) => {
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${OPENAI_API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: "what is the distance to the moon?"}],
+            max_tokens: 27,
+        })
+    }
+
+    try {
+        const response = await fetch ('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json()
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
 // run the server
-app.listen(PORT, () =>  console.log('Gipity server is running on port: ' + PORT))
+app.listen(PORT, () => console.log('Gipity server is running on port: ' + PORT))
--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,57 @@
 import { useState } from 'react'
-import reactLogo from './assets/gipity_logo.svg'
+import gipityLogo from './assets/gipity_logo.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const [message, setMessage] = useState(null)
+  const [inputValue, setInputValue] = useState(null)
+
+  // message requests from backend
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: "hello, whatsup?"
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    try {
+      const response = await fetch('http://localhost:8000/completions', options)
+      const data = await response.json()
+      console.log(data)
+      setMessage(data.choises[0].message)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  console.log(inputValue)
+
   return (
     <>
       <div>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={gipityLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>gipity</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        {/* <button onClick={() => setCount((count) => count + 1)}> */}
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          {/* Edit <code>src/App.jsx</code> and save to test HMR */}
+          <input
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          />
         </p>
+        <button onClick={getMessages}>
+          Test
+          {/* count is {count} */}
+        </button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
